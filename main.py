34e513b348388fa54297ed1021c94598dacdb829
zayn9761-eng <zayn9761@gmail.com>
2026-06-13 12:38:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,4 +16,5 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+if __name__ == "__main__":
+    app.run_polling()
