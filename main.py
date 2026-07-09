1d0ac79a35cf26f405dce9d3addb5a9c691a7da2
zayn9761-eng <zayn9761@gmail.com>
2026-07-09 20:50:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv(8213646962:AAF3EqZQuk_Ml6zA99on5q6kLzRKjr-Ntug)
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is not set in .env file")
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=8213646962:AAF3EqZQuk_Ml6zA99on5q6kLzRKjr-Ntug)
 dp = Dispatcher(bot)
 
 logging.basicConfig(level=logging.INFO)
